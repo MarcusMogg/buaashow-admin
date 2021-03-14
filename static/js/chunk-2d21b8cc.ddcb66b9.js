43a2d22298f7bf49edec3912d57b419aab2645e3
MarcusMogg <34648459+MarcusMogg@users.noreply.github.com>
2021-03-14 07:46:02 +0000

    Deploying to gh-pages from @ MarcusMogg/buaashow-admin@46fd6b7085b565f898a67690ec7a301182cf07e1 🚀

---
+++ static/js/chunk-2d21b8cc.ddcb66b9.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21b8cc"],{bff8:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},c=[],r={name:"Course"},l=r,o=t("2877"),s=Object(o["a"])(l,u,c,!1,null,null,null);n["default"]=s.exports}}]);
